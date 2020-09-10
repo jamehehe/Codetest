c7c3acc9dc271766d663e581567f549be962d9ca
Jessada Mongkol <Jessada.mong@bumail.net>
2020-09-11 04:27:03 +0700

    first commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+class TestOutput {
+	public static void main(String[] args) {
+		System.out.print("Test");
+		System.out.println(" System.out.print()");
+		System.out.print("Test System.out.println()");
+	}
+}
